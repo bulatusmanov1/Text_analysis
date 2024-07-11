130d39ca0538cab86498728968fb042cfa8471d1
Andrej Kolchin <KAAtheWise@protonmail.com>
2024-07-11 14:29:49 +0300

    sentence: add sentence splitting from AST

--- src/pipeline/sentence.py
+++ src/pipeline/sentence.py
@@ -1,22 +1,59 @@
 from nltk.tokenize import sent_tokenize
 import spacy
 
+from typing import Literal, List, Dict
 
-def split_into_sentences(text, mode):
-    if mode == "points":
-        sentences = text.split(".")
-    elif mode == "lines":
-        sentences = text.split("\n")
-    elif mode == "spaCy":
-        nlp = spacy.load("ru_core_news_sm")
-        doc = nlp(text)
-        sentences = [sent.text for sent in doc.sents]
-    elif mode == "nltk":
-        sentences = sent_tokenize(text)
-    else:
-        raise Exception('"Выберете доступный режим: points, lines, spaCy, nltk"')
+NLP = spacy.load("ru_core_news_sm")
+
+type Mode = Literal["period", "line", "spacy", "nltk"]
+
+
+def _split_into_sentences(text: str, mode: Mode) -> List[str]:
+    match mode:
+        case "period":
+            sentences = text.split(".")
+        case "line":
+            sentences = text.split("\n")
+        case "spacy":
+            sentences = [sent.text.strip() for sent in NLP(text).sents]
+        case "nltk":
+            sentences = sent_tokenize(text)
+        case _:
+            raise Exception("Выберете доступный режим: period, line, spacy, nltk")
     return sentences
 
 
-text = "Вы можете использовать любую модель для распознавания из списка.\n Для примера, воспользуемся моделью page, которая позволяет распознавать любое количество текста на изображении."  # your text
-print(split_into_sentences(text, "lines"))
+def sentences(ast: List[Dict]) -> List[Dict]:
+    out = []
+
+    heading_top = ""
+    heading_curr = ""
+
+    for el in ast:
+        match el["type"]:
+            case "heading":
+                if el["level"] == 1:
+                    heading_top = el["content"]
+                heading_curr = el["content"]
+
+                out.append(
+                    {
+                        "content": el["content"],
+                        "heading": heading_curr,
+                        "heading_top": heading_top,
+                    }
+                )
+
+            case "paragraph":
+                sentences = _split_into_sentences(el["content"])
+
+                for sentence in sentences:
+                    out.append(
+                        {
+                            "content": sentence,
+                            "heading": heading_curr,
+                            "heading_top": heading_top,
+                        }
+                    )
+
+    return out
