762c9f9fef04e11389ac4f9a7406a2403a4cd6e1
Усманов Булат <bulat.usmanov@lukoil.com>
2024-07-11 15:34:12 +0300

    quadrant

--- src/pipeline/qdrant.py
+++ src/pipeline/qdrant.py
@@ -16,7 +16,7 @@ def creating_collection(size = 128, collection_name: str = "data"):
 def add_to_collection(vectors, payloads, collection_name = "data"):
     try:
         points = []
-        next_id = len(get_all_points(1000000))
+        next_id = len(get_all_points(1000000)) + 1
 
         for vector, payload in zip(vectors, payloads):
             points.append(models.PointStruct(id=next_id, vector=vector, payload=payload))
@@ -54,7 +54,7 @@ def get_all_points(limit, collection_name = "data"):
             offset=offset
         )
 
-        points = response[0]  # Первый элемент ответа - это список точек
+        points = response[0]
         all_points.extend(points)
         
         if len(points) < limit:
@@ -63,58 +63,3 @@ def get_all_points(limit, collection_name = "data"):
         offset += limit
 
     return all_points
-    
-    
-vectors = [
-        [0.1] * 128,
-        [0.2] * 128,
-        [0.3] * 128,
-        [0.4] * 128,
-        [0.5] * 128,
-        [0.6] * 128,
-    ]
-
-payloads = [
-        {"id": 1, "data": "Первая запись"},
-        {"id": 2, "data": "Вторая запись"},
-        {"id": 3, "data": "Третья запись"},
-        {"id": 4, "data": "Третья запись"},
-        {"id": 5, "data": "Третья запись"},
-        {"id": 6, "data": "Третья запись"},
-    ]
-
-# Запрос к коллекции
-#query_vector = [0.15] * 128
-#search_result = get_from_collection(query_vector, 100)
-
-#for hit in search_result:
-    #print(f"ID: {hit.id}, Оценка: {hit.score}, Данные: {hit.payload}")
-
-
-
-vectors = [
-        [0.11] * 128,
-        [0.21] * 128,
-        [0.31] * 128,
-        [0.41] * 128,
-        [0.51] * 128,
-        [0.61] * 128,
-    ]
-
-payloads = [
-        {"id": 7, "data": "Первая запись"},
-        {"id": 8, "data": "Вторая запись"},
-        {"id": 9, "data": "Третья запись"},
-        {"id": 10, "data": "Третья запись"},
-        {"id": 11, "data": "Третья запись"},
-        {"id": 12, "data": "Третья запись"},
-    ]
-print(add_to_collection(vectors, payloads))
-print(len(get_all_points(1000)))
-
-query_vector = [0.15] * 128
-search_result = get_from_collection(query_vector, 100)
-
-for hit in search_result:
-
-    print(f"ID: {hit.id}, Оценка: {hit.score}, Данные: {hit.payload}")
