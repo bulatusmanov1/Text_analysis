603fd8651170251906735e5b080e8b371c993be2
Andrej Kolchin <KAAtheWise@protonmail.com>
2024-07-11 16:15:31 +0300

    embed: add documentation

--- src/pipeline/embed.py
+++ src/pipeline/embed.py
@@ -9,20 +9,29 @@ from typing import Literal, List, Tuple, Dict
 from .chunk import Chunk
 from ..util import sbert, yapi
 
-Mode = Literal["yandex", "sbert/paraphrase", "sbert/distiluse"]
+# Which embedding model to use
+Model = Literal["yandex", "sbert/paraphrase", "sbert/distiluse"]
+# How to process the chunk contents.
+# - "plain" leaves the content as it.
+# - "heading" prepends the current heading.
+# - "both-headings" prepends both the top level and the current headings
 EditMode = Literal["plain", "heading", "both-headings"]
 Payload = Dict
 
 
 def embed(
-    chunks: List[Chunk], mode: Mode, edit_mode: EditMode
-) -> List[Tuple[np.array, Payload]]:
+    chunks: List[Chunk], model: Model, edit_mode: EditMode
+) -> List[Tuple[np.array, Payload]]
+    """
+    Create a list of embeddings and their payloads from chunks.
+    """
+
     out = []
 
     for chunk in chunks:
         content = _edit_chunk(chunk, edit_mode)
         embed = None
-        match mode:
+        match model:
             case "yandex":
                 embed = yapi.embedding(content)
             case "sbert/paraphrase":
