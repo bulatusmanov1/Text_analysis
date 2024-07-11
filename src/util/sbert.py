e097befdd22cdd27c7c709831ace73a4f0e0be8b
Усманов Булат <bulat.usmanov@lukoil.com>
2024-07-11 16:12:43 +0300

    sbert

--- src/util/sbert.py
+++ src/util/sbert.py
@@ -4,20 +4,15 @@ import numpy as np
 from typing import Literal
 
 Model = Literal["paraphrase", "distiluse"]
-MODEL = None
-MODEL_TYPE = "paraphrase"
 
 paraphrase = SentenceTransformer("paraphrase-multilingual-MiniLM-L12-v2")
 distiluse = SentenceTransformer("distiluse-base-multilingual-cased-v1")
 
 def embed(sentence: str, model: Model) -> np.array:
-    if type(MODEL) is None or MODEL_TYPE != model:
-        match model:
-            case "paraphrase":
-                MODEL = paraphrase
-            case "distiluse":
-                MODEL = distiluse
+    match model:
+        case "paraphrase":
+            model = paraphrase
+        case "distiluse":
+            model = distiluse
 
-    return MODEL.encode(sentence)
-
-print(embed("Грету добивает телефонный звонок, где на том конце детский голос просит её соблюдать правила — у неё появляется подозрение, что дух Брамса вселился в куклу."))
+    return model.encode(sentence)
