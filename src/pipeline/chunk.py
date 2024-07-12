0ab9d5498a13f75cf9b776bacd378445bff6ed13
Andrej Kolchin <KAAtheWise@protonmail.com>
2024-07-12 12:06:47 +0300

    chunk: add window chunking

--- src/pipeline/chunk.py
+++ src/pipeline/chunk.py
@@ -2,9 +2,12 @@
 Semantic sentence chunking
 """
 
+from scipy.spatial import distance
+
 from typing import List, Literal, Dict
 
 from .sentence import Sentence
+from ..util import yapi
 
 Mode = Literal[
     "sentence",
@@ -20,6 +23,8 @@ def chunk(sentences: List[Sentence], mode: Mode) -> List[Chunk]:
             return _chunk_sentences(sentences)
         case "section":
             return _chunk_sections(sentences)
+        case "window":
+            return _chunk_window(sentences)
 
 
 def _chunk_sentences(sentences: List[Sentence]) -> List[Chunk]:
@@ -78,6 +83,40 @@ def _chunk_sections(sentences: List[Sentence]) -> List[Chunk]:
     return out
 
 
-def _chunk_window(sentences: List[Sentence]) -> List[Chunk]:
-    # TODO
-    pass
+def _chunk_window(sentences: List[Sentence], threshold: float = 0.3) -> List[Chunk]:
+    out = []
+
+    heading = None
+    line_start = 0
+    page_start = 0
+    line_end = 0
+    page_end = 0
+    content = ""
+
+    for sentence in sentences:
+        if content == "":
+            heading = sentence["heading"]
+            line_start = sentence["line"]
+            page_start = sentence["page_start"]
+            content = sentence["content"]
+
+        embed = yapi.embedding(content)
+        curr_embed = yapi.embedding(sentence["content"])
+
+        if distance.cosine(embed, curr_embed) > threshold:
+            out.append(
+                {
+                    "content": content,
+                    "heading": heading,
+                    "line_start": line_start,
+                    "line_end": line_end,
+                    "page_start": page_start,
+                    "page_end": page_end,
+                }
+            )
+        else:
+            content += sentence["content"]
+            line_end = sentence["line"]
+            page_end = sentence["page"]
+
+    return out
