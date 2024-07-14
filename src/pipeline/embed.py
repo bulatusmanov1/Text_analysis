16bc14ad1c929f7b60cd60f750ff8d45b4166375
Andrej Kolchin <KAAtheWise@protonmail.com>
2024-07-14 16:32:34 +0300

    embed: make embedding async

--- src/pipeline/embed.py
+++ src/pipeline/embed.py
@@ -3,7 +3,9 @@ Chunk embedding
 """
 
 import numpy as np
+import httpx
 
+import asyncio
 from typing import Literal, List, Tuple, Dict
 
 from .chunk import Chunk
@@ -21,23 +23,36 @@ def embed(chunks: List[Chunk], mode: Mode) -> List[Tuple[np.array, Payload]]:
     Create a list of embeddings and their payloads from chunks.
     """
 
-    out = []
+    async def a():
+        tasks = []
 
-    for chunk in chunks:
-        content = _edit_chunk(chunk, mode)
-        embed = yapi.embedding(content)
+        async with httpx.AsyncClient() as client:
+            async with asyncio.TaskGroup() as tg:
+                for chunk in chunks:
+                    task = tg.create_task(_embed_chunk(chunk, mode))
+                    tasks.append(task)
 
-        payload = {
-            "content": chunk["content"],
-            "line_start": chunk["line_start"],
-            "line_end": chunk["line_end"],
-            "page_start": chunk["page_start"],
-            "page_end": chunk["page_end"],
-            "document": chunk["document"],
-        }
-        out.append((embed, payload))
+        return tasks
+
+    tasks = asyncio.run(a())
+    out = [task.result() for task in tasks]
 
     return out
+
+
+async def _embed_chunk(chunk, mode) -> Tuple[np.array, Payload]:
+    content = _edit_chunk(chunk, mode)
+    embed = yapi.embedding(content)
+
+    payload = {
+        "content": chunk["content"],
+        "line_start": chunk["line_start"],
+        "line_end": chunk["line_end"],
+        "page_start": chunk["page_start"],
+        "page_end": chunk["page_end"],
+        "document": chunk["document"],
+    }
+    return (embed, payload)
 
 
 def _edit_chunk(chunk: Chunk, mode: Mode) -> str:
