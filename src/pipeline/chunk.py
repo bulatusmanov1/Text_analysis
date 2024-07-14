2111bcb343d1344543a9661a209f454ceec454f6
Andrej Kolchin <KAAtheWise@protonmail.com>
2024-07-14 13:42:37 +0300

    chunk: add fixed-size chunking

--- src/pipeline/chunk.py
+++ src/pipeline/chunk.py
@@ -9,12 +9,14 @@ from typing import List, Literal, Dict
 from .sentence import Sentence
 from ..util import yapi
 
-Mode = Literal["section"]
+Mode = Literal["section", "size-256"]
 Chunk = Dict
 
 
 def chunk(sentences: List[Sentence], mode: Mode) -> List[Chunk]:
     match mode:
+        case "size-256":
+            return _chunk_size(sentences, size=256)
         case "section":
             return _chunk_sections(sentences)
 
@@ -54,5 +56,43 @@ def _chunk_sections(sentences: List[Sentence]) -> List[Chunk]:
 
             heading = sentence["heading"]
             content = sentence["heading"]
+
+    return out
+
+def _chunk_size(sentences: List[Sentence], size: int) -> List[Chunk]:
+    out = []
+
+    heading = None
+    line_start = 0
+    page_start = 0
+    line_end = 0
+    page_end = 0
+    content = ""
+
+    for sentence in sentences:
+        if heading is None:
+            heading = sentence["heading"]
+            line_start = sentence["line"]
+            page_start = sentence["page"]
+
+        if len(content) + len(sentence["content"]) < size:
+            content += f"{sentence['content']} "
+            line_end = sentence["line"]
+            page_end = sentence["page"]
+        else:
+            out.append(
+                {
+                    "content": content,
+                    "heading": heading,
+                    "line_start": line_start,
+                    "line_end": line_end,
+                    "page_start": page_start,
+                    "page_end": page_end,
+                    "document": sentence["document"],
+                }
+            )
+
+            heading = sentence["heading"]
+            content = sentence["content"]
 
     return out
