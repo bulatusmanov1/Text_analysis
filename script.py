498c934c1fe0dbb5a02d05207bcecb4b7a8728d3
Andrej Kolchin <KAAtheWise@protonmail.com>
2024-07-15 13:54:54 +0300

    script: add test matcher function

--- script.py
+++ script.py
@@ -1,4 +1,3 @@
-from pprint import pp
 import sys
 import os
 import json
@@ -45,3 +44,42 @@ match sys.argv[1]:
         )
 
         print(qdrant.dump_points(points))
+
+    case "bench":
+        pass
+
+
+def matcher(test, result) -> int | None:
+    MIN, MAX = 0, 1000
+
+    distance = None
+
+    for answer in test["answer"]:
+        if answer["page"] == result["payload"]["page_start"]:
+            line_start = result["payload"]["line_start"]
+
+            if answer["page"] == result["payload"]["page_end"]:
+                line_end = result["payload"]["line_end"]
+            else:
+                line_end = MAX
+
+        elif answer["page"] == result["payload"]["page_end"]:
+            line_end = result["payload"]["line_end"]
+
+            if answer["page"] == result["payload"]["page_end"]:
+                line_start = result["payload"]["line_start"]
+            else:
+                line_start = MIN
+
+        else:
+            # no match for this answer
+            continue
+
+        if line_start <= answer["line"] <= line_end:
+            distance = 0
+        else:
+            distance = min(
+                abs(answer["line"] - line_start), abs(answer["line"] - line_end)
+            )
+
+    return distance
