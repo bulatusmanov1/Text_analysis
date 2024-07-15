20c712afeb6c074ec12d7253bfba0faccdafe72b
Andrej Kolchin <KAAtheWise@protonmail.com>
2024-07-15 13:21:18 +0300

    search: create module

---
+++ src/search.py
@@ -0,0 +1,11 @@
+from util import qdrant
+from pipeline.chunk import Mode as ChunkMode
+from pipeline.embed import Mode as EmbedMode
+
+
+def search(
+    query: str, chunk_mode: ChunkMode, embed_mode: EmbedMode, document: int = None
+):
+    results = qdrant.search(
+        query, collection=f"{chunk_mode}+{embed_mode}", document=document
+    )
